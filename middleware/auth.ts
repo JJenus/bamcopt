f48832174b58e3860ad9736d4556128d39308530
jjenus <biz.jenus@gmail.com>
2024-06-27 21:59:40 +0100

    auth restore

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -7,15 +7,15 @@ export default defineNuxtRouteMiddleware((to, from) => {
 	// In a real app you would probably not redirect every route to `/`
 	// however it is important to check `to.path` before redirecting or you
 	// might get an infinite redirect loop
-	const cookie = useCookie<AuthToken | null | undefined>("auth");
+	const cookie = useCookie<AuthToken | any>("auth");
 	if (cookie.value == null || cookie.value == undefined) {
 		return navigateTo("sign-in");
 	}
 
-	// if (
-	// 	!useAuth().isAuthenticated() ||
-	// 	useAuth().userData.value?.user.userType !== "user"
-	// ) {
-	// 	return useAuth().logout();
-	// }
+	if (
+		!useAuth().isAuthenticated() ||
+		useAuth().userData.value?.user.userType !== "user"
+	) {
+		return useAuth().logout();
+	}
 });
